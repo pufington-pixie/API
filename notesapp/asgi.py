39c5491f203d438c5cb9f5b9d5671281560e9742
pufington <ganesh289katta@gmail.com>
2024-02-19 11:00:09 +0530

    .

--- notesapp/asgi.py
+++ notesapp/asgi.py
@@ -3,7 +3,7 @@ ASGI config for notesapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this file, sesssssse
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
